0d4ffcb9a23390155ed0670eec15d7991a76bf64
Soumik Mukherjee <Soumik.Mukherjee@hyland.com>
2023-06-08 14:57:08 +0530

    initial commit

---
+++ books-app/Data/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace my_books.Data.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                        
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
